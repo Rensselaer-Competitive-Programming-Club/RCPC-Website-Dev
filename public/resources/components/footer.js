22653981f287ed8612cb58e5c3dc15f3017c60f4
hemanskolla <33156254+hemanskolla@users.noreply.github.com>
2025-02-07 16:02:30 -0500

    Structure Header & Footer JS + Structure All Styling
    Done:
    - IA Changes
    - Basic Setup Before JS Implementations
    - Setup Styling
    - Add Color Palette in theme.css
    - Reformat Home Page
    - Add Logos
    Pending:
    - Update Logos when Received
    - Finish Header and Footer React Components
    - Implement base.css & layout.css & components.css

---
+++ public/resources/components/footer.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function Footer() {
+    return React.createElement(
+        
+    );
+}
+
+ReactDOM.createRoot(document.querySelector("footer"))?.render(React.createElement(Footer));
---
+++ public/resources/components/header.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function Header() {
+    return React.createElement(
+        
+    );
+}
+
+ReactDOM.createRoot(document.querySelector("header"))?.render(React.createElement(Header));
