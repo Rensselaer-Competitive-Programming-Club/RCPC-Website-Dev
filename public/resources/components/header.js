52bce06893d2054f42830eec2bb8e34db5a18fac
hemanskolla <33156254+hemanskolla@users.noreply.github.com>
2025-02-07 17:29:20 -0500

    Header Implementation
    Done:
    - Logo to Navigate Home
    - Navigation to Each Page
    - IA Changes
    - Skeletal Events, Problems, and About Pages & their respective CSS
    - Style Header
    Pending:
    - On-Hover & On-Select Styling (+ Other Dynamic Styling)

--- public/resources/components/header.js
+++ public/resources/components/header.js
@@ -1,8 +1,16 @@
 'use strict';
 
 function Header() {
-    return React.createElement(
-        
+    return (
+        <header>
+            <a href="/"><img src="/resources/rcpc_white.png" alt="&gt;_RCPC"></img></a>
+
+            <nav>
+                <a href="/pages/events/events.html">Events</a>
+                <a href="/pages/problems/problems.html">Problems</a>
+                <a href="/pages/about/about.html">About</a>
+            </nav>
+        </header>
     );
 }
 
