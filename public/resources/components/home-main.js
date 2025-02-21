fd98a2285896be68d3ff66f527383671b8d852e6
hemanskolla <33156254+hemanskolla@users.noreply.github.com>
2025-02-21 16:42:54 -0500

    Homepage: File Setup + Link CSS

---
+++ public/resources/components/home-main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function Home() {
+    return (
+        <main>
+            
+        </main>
+    );
+}
+
+ReactDOM.createRoot(document.querySelector("main"))?.render(React.createElement(Home));
