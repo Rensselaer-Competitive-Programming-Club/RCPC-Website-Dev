25e847fef9a8060b5149dcff9ec1ed737963fba0
M-Partridge <partrm2@rpi.edu>
2025-02-25 17:30:27 -0500

    New implementation of problems body
    - added dynamic button system to add 1 button for each problem with a onclick function to update textarea with problem description
    - created a renderApp function to re-render the page so that when a global variable is changed it properly displays

--- public/pages/problems/problems.js
+++ public/pages/problems/problems.js
@@ -1,13 +1,52 @@
 'use strict';
 
+let problems = [
+    {
+        name: "Problem 1",
+        description: "Problem 1 Description",
+        rating: "1900"
+    },
+    {
+        name: "Problem 2",
+        description: "Problem 2 Description",
+        rating: "1200"
+    }
+]
+
+let selectedProblem = "t";
+
+function onProblemButtonClick(description) {
+    selectedProblem = description;
+    renderApp();
+}
+
 function ProblemBody() {
+
+
     return (
         <main className="problem-body">
-            <textarea className="problem-box" value="Problem Box"></textarea>
+            <div className="problem-buttons">
+                {problems.map((problem, index) => (
+                    <button className="problem-button" key={index} onClick={() => onProblemButtonClick(problem.description)}>
+                        {problem.name}
+                    </button>
+                ))}
+            </div>
+            <textarea className="problem-box" value={selectedProblem} readOnly></textarea>
         </main>
     );
 }
 
+
+// render function
+function renderApp() {
+    const rootElement = document.querySelector("main");
+    if (rootElement) {
+        ReactDOM.createRoot(rootElement).render(<ProblemBody />);
+    } else {
+        console.error("No <main> element found!");
+    }
+}
+
+// Initial render
 ReactDOM.createRoot(document.querySelector("main"))?.render(<ProblemBody />);
-
-
