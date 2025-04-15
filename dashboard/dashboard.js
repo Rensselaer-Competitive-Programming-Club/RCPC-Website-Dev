58db5f00d63a705a0ec340ec5741d6a55c93143b
M-Partridge <partrm2@rpi.edu>
2025-04-15 14:39:06 -0400

    Trying to get dashboard page to link
    pushing this because the page is not linking for some reason

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -3,39 +3,38 @@
 function Achievement() {
     return (
         <form action="/admin" method="post">
-            <label for="event-name">Event Name:</label>
+            <label htmlFor="event-name">Event Name:</label>
             <input type="text" id="event-name" name="event-name"/>
             <br/>
-            <label for="photo">Upload Photo:</label>
+            <label htmlFor="photo">Upload Photo:</label>
             <input type="file" id="photo" name="photo"/>
             <br/>
-            <label for="website" method="post">Website URL:</label>
+            <label htmlFor="website">Website URL:</label>
             <input type="url" id="website" name="website"/>  
             <br/>
-            <label for="location" method="post">Location:</label>
+            <label htmlFor="location">Location:</label>
             <input type="text" id="location" name="location"/>
             <br/>
-            <label for="description" method="post">Description:</label>
+            <label htmlFor="description">Description:</label>
             <input type="text" id="description" name="description"/>
             <br/>
-            <label for="what-we-won" method="post">What We Won</label>
-            <input type="test" id="what-we-won" name="what-we-won"/>
+            <label htmlFor="what-we-won">What We Won</label>
+            <input type="text" id="what-we-won" name="what-we-won"/>
         </form>
     );
 }
 
-function Main() {
+function Dashboard() {
     const [showAchievement, setShowAchievement] = React.useState(false)
 
-    return <section>
-
-        {/* TO BE MOVED TO ANOTHER FILE */}
+    return <main>Test
+        {/*
         <button onClick={() => {
             setShowAchievement(!showAchievement)
         }}>Achievement</button>
 
-        { showAchievement && <Achievement /> }
-    </section>
+        { showAchievement && <Achievement /> }*/}
+    </main>
 }
 
-ReactDOM.createRoot(document.querySelector("main"))?.render(<Main />);
+ReactDOM.createRoot(document.querySelector("main"))?.render(<Dashboard />);
