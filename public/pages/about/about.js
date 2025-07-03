c8639bf156530af3bfe33cfde51a11f55246c754
jaocbhebbel <jacob.hebbel@gmail.com>
2025-07-03 11:02:23 -0400

    documented the html and js files for about; the css was already documented at a good standard

--- public/pages/about/about.js
+++ public/pages/about/about.js
@@ -18,61 +18,85 @@ function Card({src, name, title}) {
     </div >
 }
 
-/*generates a card component for every name in the json*/
-/*in the future, data should be fetched from db*/
+/*fetches team data from mongo and inserts Member components*/
 function MeetTheTeam() {
     
     console.log("Meet the team called");
 
+    /* 
+       state depends on result of db call
+       loading is initially true bc duh
+       leave members empty and error null
+       we will update states after db call returns
+    */
     const [members, setMembers] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
-    const imagePath = "/resources/images/people/";
+    const imagePath = "/resources/images/people/"; // improvement: make these images remotely hosted
 
     // useEffect handles the async nature of the db call
     // tracks if the call succeeds/fails, and properly
     // updates members/loading/error with whatever happens
-    useEffect(() => {
+    
+    useEffect(() => { // the (() => {}) syntax means this function will run immediately
         async function fetchTeamMembers() {
             try {
-                const response = await fetch('/database/directors?isActive=true');
+                const response = await fetch('/database/directors?isActive=true'); // db call done here
                 if (!response.ok) {
-                    throw new Error(`HTTP Error! Status ${response.status}`);
+                    throw new Error(`HTTP Error! Status ${response.status}`); // result of bad response; implies error state is true
                 }
-
+                
+                // reaching here in execution means no errors
+                // pull team data from the response object
                 const board = await response.json();
                 const persons = board.data.map(member => ({
                     src: imagePath + member["rcs"] + ".png",
                     name: member["name"],
                     title: member["role"]
                 }));
+                // set the members state and remove loading state
                 setMembers(persons);
                 setLoading(false);
             } catch(error) {
+                // catch is triggered by an error (from !response.ok or otherwise)
+                // set error state with the error message and remove loading state
                 setError(error.message);
                 setLoading(false);
             }
         }
-
+        
+        // call the function we just defined
         fetchTeamMembers();
     }, []); // empty array means function runs more than once (in the event of loading outcome)
 
 
     console.log("async db call finished");
     if (loading) {
+        // text for loading state
         return <div>Loading team members ...</div>
     }
 
     if (error) {
+        // text for error state
         return <div>Error with loading team members: {error.message}</div>
     }
     
+    /*
+       logic for building team cards. parse prez / vp data with keyword; all other roles are 'directors'
+    */
     const presidents = members.filter(member => member.title.includes("President")); // Example filtering
     const directors = members.filter(member => member.title.includes("Director") && !member.title.includes("President"));
 
+    /*
+       the tier of directors is significant.
+       first row of directors are the internally-facing positions: Logisitics, Finance, Resources (maybe changed to Education?)
+       second row of directors are the externally-facing positions: Sponsorship, Marketing, Technology (tech is external bc rcos?)
+    */
     var directors1 = directors.filter(member => !member.name.includes("Heman") && !member.name.includes("Jacob"));
     var directors2 = directors.filter(member => member.name.includes("Heman") || member.name.includes("Jacob"));
 
+
+    /* builds the div with the parsed mongo data */
     return <div className="card-container-container"> 
         
         {/*inserts presidents items into page*/}
@@ -98,6 +122,7 @@ function MeetTheTeam() {
     </div>
 }
 
+/* root function that calls the others. to add another section to this page, put a new div under <section> and add a function with the same name */
 function Main() {
     return <section className="meet-the-team">
         <TeamHeader />
@@ -105,4 +130,5 @@ function Main() {
     </section>
 }
 
+/* I don't know if function names have to be capitalized I was going off of online examples :sobs: */
 ReactDOM.createRoot(document.querySelector("main"))?.render(<Main />);
